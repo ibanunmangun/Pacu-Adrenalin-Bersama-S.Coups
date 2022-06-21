722fbb9fc8e5eeb3b02b2a0d9574c71ebf02d84a
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2022-06-21 21:02:38 +0800

    Add files via upload

---
+++ data/Points_2.js
@@ -0,0 +1,11 @@
+var json_Points_2 = {
+"type": "FeatureCollection",
+"name": "Points_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Jomblang Cave", "desc": "Jomblang Cave, Jetis, Pacarejo, Semanu, Gunung Kidul Regency, Special Region of Yogyakarta 55893, Indonesia", "X": "110.6383307", "Y": "-8.0281562", "no": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.6383307, -8.0281562 ] } },
+{ "type": "Feature", "properties": { "name": "Gumuk Pasir Parangkusumo", "desc": "Parangkusumo Sand Dunes, Jl. Pantai Parangkusumo RT. 1 Grogol 10, Parangtritis, Kec. Kretek, Kabupaten Bantul, Daerah Istimewa Yogyakarta 55772, Indonesia", "X": "110.3191217", "Y": "-8.0190343", "no": "2" }, "geometry": { "type": "Point", "coordinates": [ 110.3191217, -8.0190343 ] } },
+{ "type": "Feature", "properties": { "name": "Pantai Parangtritis", "desc": "Parangtritis Beach, Special Region of Yogyakarta, Indonesia", "X": "110.3298045", "Y": "-8.024608", "no": "3" }, "geometry": { "type": "Point", "coordinates": [ 110.3298045, -8.024608 ] } },
+{ "type": "Feature", "properties": { "name": "TapHouse Beer Garden", "desc": "TapHouse Beer Garden, Jl. Jlagran No.18, Pringgokusuman, Gedong Tengen, Yogyakarta City, Special Region of Yogyakarta 55272, Indonesia", "X": "110.3605329", "Y": "-7.7902883", "no": "4" }, "geometry": { "type": "Point", "coordinates": [ 110.3605329, -7.7902883 ] } }
+]
+}
